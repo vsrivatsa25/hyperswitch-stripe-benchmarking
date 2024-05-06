06c5f539e27e4e8cea0ec3e00795f9f7234cd2e6
Vrishab Srivatsa <vrishab.srivatsa@juspay.in>
2024-05-06 18:19:13 +0530

    Test commit

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,20 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
+        <Script src="https://example.com/script.js" />
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Yes started by editing <code>pages/index.js</code>
         </p>
       </main>
-
-      <Footer />
     </div>
-  )
+  );
 }
